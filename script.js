f6cca8597758c66a4565c27e2407c2b195ceee4c
FahadAlHossain <fahadalhossain5@gmail.com>
2024-12-22 16:53:35 +0600

    details page completed

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const close = document.getElementById("close");
 
+const mainImg = document.getElementById("mainImg");
+const smallImg = document.getElementsByClassName("small-img");
+
 if(bar){
     bar.addEventListener('click' , () => {
         nav.classList.add("active");
@@ -12,4 +15,17 @@ if(close){
     close.addEventListener('click' , () => {
         nav.classList.remove("active");
     })
+}
+
+smallImg[0].onclick = () => {
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = () => {
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = () => {
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = () => {
+    mainImg.src = smallImg[3].src;
 }
